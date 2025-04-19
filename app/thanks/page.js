c4898294950b393f4fea61c10d45f6e61a6fc3e2
agung343 <agungwahydi343@gmail.com>
2025-04-19 11:10:49 +0700

    update-thanks

--- app/thanks/page.js
+++ app/thanks/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function ThanksPage() {
   return (
-    <div className="min-h-[800px]">
+    <div className="min-h-[800px] mt-12 flex flex-col items-center">
       <h1 className="text-center text-cherry-red text-5xl">
         Thank you for ordering, please wait while we delivery to you.
       </h1>
+      <button type="button" className="py-2 px-4 bg-medium-blue text-background text-xl rounded-lg hover:bg-light-blue">
+        <Link href="/">Back to Home</Link>
+      </button>
     </div>
   );
 }
